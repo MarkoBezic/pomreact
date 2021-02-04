276b15372f727ed896c6ee29edcbc53b64e8c916
markobezic <marko.bezic@gmail.com>
2021-02-04 15:58:21 -0500

    updated styles of editTaskRecord component

--- src/components/EditTaskRecord.js
+++ src/components/EditTaskRecord.js
@@ -78,9 +78,6 @@ class EditTaskRecord extends Component {
     })
   }
 
-  // startInputRef = React.createRef()
-  // endInputRef = React.createRef()
-
   handleInputChange = e => {
     const target = e.target
     const value = target.value
@@ -121,86 +118,88 @@ class EditTaskRecord extends Component {
 
   render() {
     return (
-      <div>
-        {this.state.warning ? <p>Cannont have empty field</p> : ''}
-        <table>
-          <thead>
-            <tr>
-              <th>Task</th>
-              <th className="pr-3">Start Time</th>
-              <th>End Time</th>
-              <th>Delete</th>
-            </tr>
-            {this.state.taskRoundsList.map((task, index) => (
-              <tr key={index}>
-                {this.state.currentTaskId === task.parentTaskId ? (
-                  <>
-                    <td>{task.taskName} </td>
-                    <td className="text-center">{task.startTime}</td>
-                    <td className="text-center">{task.endTime}</td>
-                    <td>
-                      <button
-                        className="btn btn-primary ml-1 mr-1 mt-1"
-                        onClick={e => {
-                          this.handleDeleteRound(e, task.id)
-                        }}
-                      >
-                        Delete
-                      </button>
-                    </td>
-                  </>
-                ) : (
-                  ''
-                )}
+      <div className="d-flex ">
+        <div className="border border-light rounded d-block mx-auto mt-5 p-4">
+          {this.state.warning ? <p>Cannont have empty field</p> : ''}
+          <table class="text-white">
+            <thead>
+              <tr className="border bg-secondary">
+                <th className="p-2">Task</th>
+                <th className="px-3">Start Time</th>
+                <th className="px-3">End Time</th>
+                <th className="px-3">Delete</th>
               </tr>
-            ))}
-            {this.state.isEidtable ? (
-              <tr>
-                <td>{this.state.taskName}</td>
-                <td>
-                  <input
-                    className="m-1"
-                    type="datetime-local"
-                    name="startTime"
-                    placeholder="mm/dd/yyy, hh:mm:ss AM/PM"
-                    onChange={this.handleInputChange}
-                    // ref={this.startInputRef}
-                  />
-                </td>
-                <td>
-                  <input
-                    className="m-1"
-                    type="datetime-local"
-                    name="endTime"
-                    placeholder="mm/dd/yyy, hh:mm:ss AM/PM"
-                    onChange={this.handleInputChange}
-                    // ref={this.endInputRef}
-                  />
-                </td>
-                <td>
-                  <button
-                    className="btn btn-primary ml-1 mr-1 mt-1"
-                    onClick={() => {
-                      this.handleSaveRound()
-                    }}
-                  >
-                    Save
-                  </button>
-                </td>
-              </tr>
-            ) : (
-              ''
-            )}
-          </thead>
-        </table>
-        <button
-          className="btn btn-primary ml-1 mr-1 mt-1"
-          onClick={() => {
-            this.handleAddRound()
-          }}
-        >
-          Add Round
-        </button>
+              {this.state.taskRoundsList.map((task, index) => (
+                <tr key={index}>
+                  {this.state.currentTaskId === task.parentTaskId ? (
+                    <>
+                      <td className="px-3">{task.taskName} </td>
+                      <td className="text-center px-3">{task.startTime}</td>
+                      <td className="text-center px-3">{task.endTime}</td>
+                      <td>
+                        <button
+                          className="btn btn-primary mx-1 mt-2"
+                          onClick={e => {
+                            this.handleDeleteRound(e, task.id)
+                          }}
+                        >
+                          Delete
+                        </button>
+                      </td>
+                    </>
+                  ) : (
+                    ''
+                  )}
+                </tr>
+              ))}
+              {this.state.isEidtable ? (
+                <tr>
+                  <td>{this.state.taskName}</td>
+                  <td>
+                    <input
+                      className="m-1"
+                      type="datetime-local"
+                      name="startTime"
+                      placeholder="mm/dd/yyy, hh:mm:ss AM/PM"
+                      onChange={this.handleInputChange}
+                      // ref={this.startInputRef}
+                    />
+                  </td>
+                  <td>
+                    <input
+                      className="m-1"
+                      type="datetime-local"
+                      name="endTime"
+                      placeholder="mm/dd/yyy, hh:mm:ss AM/PM"
+                      onChange={this.handleInputChange}
+                      // ref={this.endInputRef}
+                    />
+                  </td>
+                  <td>
+                    <button
+                      className="btn btn-primary ml-1 mr-1 mt-1"
+                      onClick={() => {
+                        this.handleSaveRound()
+                      }}
+                    >
+                      Save
+                    </button>
+                  </td>
+                </tr>
+              ) : (
+                ''
+              )}
+            </thead>
+          </table>
+          <button
+            className="btn btn-primary mx-1 mt-3"
+            onClick={() => {
+              this.handleAddRound()
+            }}
+          >
+            Manually add round
+          </button>
+        </div>
       </div>
     )
   }
